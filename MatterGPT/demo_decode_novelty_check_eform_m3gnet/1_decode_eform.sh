931b3b0a1b4b3aa0a3c77fc17b082eaaabc04365
YJC-log <1529639249@qq.com>
2025-12-19 12:59:33 +0800

    对解码后的novelty_check进行了优化，现在是SQlite查表方式进行新颖性检查，加速比100x plus

--- MatterGPT/demo_decode_novelty_check_eform_m3gnet/1_decode_eform.sh
+++ MatterGPT/demo_decode_novelty_check_eform_m3gnet/1_decode_eform.sh
@@ -3,7 +3,6 @@
 # -------------------------------------
 # 描述: 调用 run.py 脚本解码SLICES并且计算新材料的novelty
 # -------------------------------------
-
 python run.py \
     --input_csv ../1_train_generate/eform.csv \
     --structure_json ../0_dataset/cifs_filtered.json \
